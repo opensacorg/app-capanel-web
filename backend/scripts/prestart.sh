0a200d78862a2e8fc491949eacb15b36bd818353
Nate Bass <nw3455@gmail.com>
2025-11-07 05:25:06 -0800

    Fix the Docker build error related to prestart.sh.
    * Revert changes to the full stack FastAPI template scripts.

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -4,10 +4,10 @@ set -e
 set -x
 
 # Let the DB start
-python backend_pre_start.py
+python app/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python initial_data.py
+python app/initial_data.py
