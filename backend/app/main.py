0726ea208c5a9dba02ef3a9de487574e5a11fd12
Nate Bass <nw3455@gmail.com>
2026-02-27 01:12:39 -0800

    Uses Nginx to proxy the frontend and backend.
    * Allow common FastAPI base routes like docs and redoc etc. to the backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    openapi_url="/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
 )
 
